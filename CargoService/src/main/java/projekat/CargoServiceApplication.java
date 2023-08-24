0e329f87323695170a82414f470683ae840966b0
Nadja <j.nadja97@gmail.com>
2023-08-24 15:30:47 +0200

    Initial commit for eureka server and eureka client and connection to databases

---
+++ CargoService/src/main/java/projekat/CargoServiceApplication.java
@@ -0,0 +1,13 @@
+package projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoServiceApplication.class, args);
+	}
+
+}
